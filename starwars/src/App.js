a5b7d9419d4c2197f9afd90e4cf9dcdaa94ceb1f
Christopher Bailey <seabayley@outlook.com>
2019-09-13 12:20:14 -0500

    Added card rendering and test for crash

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterCardsWrapper from './components/CharacterCardsWrapper';
+import { dummyData } from './dummydata';
 import './App.css';
 
 const App = () => {
+  const [swapiData, setSwapiData] = useState(null);
+  const [loading, setLoading] = useState(true);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +14,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  function useAxios() {
+
+    useEffect(() => {
+      axios.get(`https://swapi.co/api/people`)
+        .then(function (response) {
+          setSwapiData(swapiData => swapiData = response.data);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }, [])
+  }
+
+  useAxios()
+
+  if (loading) return <p>Loading...</p>
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharacterCardsWrapper data={swapiData.results} />
     </div>
   );
 }
